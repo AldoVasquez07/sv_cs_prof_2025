c1a6ad27ded2986332f8e45ffb784a3257663957
RedzukiVei <jquispel@ulasalle.edu.pe>
2025-10-14 22:32:51 -0500

    Empezando con la codificación del sistema de predicción clínicas e imagenes, a la vez definiciedo la predicción basada en datos clínicos

--- app.py
+++ app.py
@@ -23,3 +23,17 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max
+
+# Clase del sistema híbrido
+class HybridStrokePredictor:
+    """"Sistema hídrido que combina predicciones clínicas e imágenes"""
+    def __init__(self, clinical_model, cnm_model, scaler, label_encoders):
+        self.clinical_model = clinical_model
+        self.cnn_model = cnm_model
+        self.scaler = scaler
+        self.label_encoders = label_encoders
+
+    def predict_clinical(self, clinical_data):
+        """"Predicció basada en datos clínicos"""
+        clinical_scaled = self.sacler.transform(clinical_data)
+        return self.clinical_model.predict_proba(clinical_scaled)[:, 1]
